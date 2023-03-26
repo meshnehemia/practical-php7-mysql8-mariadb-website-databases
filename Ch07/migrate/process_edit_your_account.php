f06f5f51f81a85c6149f38986ca416811fa4d7d0
meshnehemia <meshnehemia7@gmail.com>
2023-03-26 22:22:33 +0300

    no changes

--- Ch07/migrate/process_edit_your_account.php
+++ Ch07/migrate/process_edit_your_account.php
@@ -169,6 +169,4 @@ if (mysqli_num_rows($result) == 1) { // Valid user ID, display the form.
 	// Get the user's information:
 	$row = mysqli_fetch_array ($result, MYSQLI_ASSOC);
 	// Create the form: 	#7
-	
-	
 ?>
